6ad4d2fe706e2dac26e6dd72123940361058063d
Ryan Karl <ryan@brightcloudengineering.com>
2016-02-08 21:30:17 +0000

    More work on navigation.

---
+++ config/login_navigation.rb
@@ -0,0 +1,15 @@
+SimpleNavigation::Configuration.run do |navigation|
+  navigation.items do |primary|
+    if !user_signed_in?
+      primary.item :login, 'Login', new_user_session_path
+      primary.item :signup, "Sign Up", new_user_registration_path
+    else
+      primary.item :cur_user, current_user.email do |actions|
+        actions.item :edit_pwd, 'Edit Password', edit_user_password_path 
+        actions.item :logout, 'Logout', destroy_user_session_path, :method => :delete
+      end
+    end
+    primary.dropdown = true
+    primary.dom_class = 'nav navbar-nav navbar-right'
+  end
+end
